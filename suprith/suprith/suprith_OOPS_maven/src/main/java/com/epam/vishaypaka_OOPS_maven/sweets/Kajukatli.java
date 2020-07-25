b82de2e224edd6070518c3e30d587873e64da89a
suprithreddy267 <52496136+suprithreddy267@users.noreply.github.com>
2020-07-25 19:12:53 +0530

    Add files via upload

---
+++ suprith/suprith/suprith_OOPS_maven/src/main/java/com/epam/vishaypaka_OOPS_maven/sweets/Kajukatli.java
@@ -0,0 +1,9 @@
+package com.epam.suprith_OOPS_maven.sweets;
+
+public class Kajukatli extends Sweets 
+{
+    public Kajukatli(String name, int price, int weight) 
+   {
+        super(name, price, weight);
+    }
+}
---
+++ suprith/suprith/suprith_OOPS_maven/src/main/java/com/epam/vishaypaka_OOPS_maven/sweets/Rasgulla.java
@@ -0,0 +1,9 @@
+package com.epam.suprith_OOPS_maven.sweets;
+
+public class Rasgulla extends Sweets 
+{
+    public Rasgulla(String name, int price, int weight) 
+   {
+        super(name, price, weight);
+    }
+}
